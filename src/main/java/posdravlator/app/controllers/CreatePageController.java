48a86ab2781936afd0420937fab5af60c98f9a08
Sergey Sobolev <cybersobic@gmail.com>
2025-01-23 10:50:01 +0300

    Add local date to header on web page

--- src/main/java/posdravlator/app/controllers/CreatePageController.java
+++ src/main/java/posdravlator/app/controllers/CreatePageController.java
@@ -4,10 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import posdravlator.app.models.Birthday;
 import posdravlator.app.services.BirthdayService;
 
 @Controller
--- src/main/java/posdravlator/app/controllers/MainPageController.java
+++ src/main/java/posdravlator/app/controllers/MainPageController.java
@@ -1,14 +1,20 @@
 package posdravlator.app.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import posdravlator.app.services.MainPageService;
 
 @Controller
 public class MainPageController {
+    @Autowired
+    private MainPageService mainPageService;
+
     @GetMapping
     public String getMainPage(Model model) {
         model.addAttribute("title", "Поздравлятор - Главная страница");
+        model.addAttribute("localDate", "Сегодня: " + mainPageService.getDate());
         return "main";
     }
 }
